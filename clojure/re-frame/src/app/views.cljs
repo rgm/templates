4930da6c29f9173f247db0df023eb9abed13576f
Ryan McCuaig <ryan@ryanmccuaig.net>
2024-10-01 18:45:48 +0000

    Stop re-making the handler every refresh

--- clojure/re-frame/src/app/views.cljs
+++ clojure/re-frame/src/app/views.cljs
@@ -4,11 +4,12 @@
    [re-frame.core :as rf]))
 
 (defn Root []
-  (let [*n (rf/subscribe [:current-counter-value])]
+  (let [*n (rf/subscribe [:current-counter-value])
+        handle-click (fn [_ev] (rf/dispatch [:increment]))]
     (fn []
       [:main
        [:div "counter is: " [:strong (deref *n)]]
        [:div
         [:button
-         {:on-click (fn [_ev] (rf/dispatch [:increment]))}
+         {:on-click handle-click}
          "increment"]]])))
